918e921c7ebf760aed5d3d4cf2b3d49a2a8d0272
ishitaa dhingra <110631113+ishitaa-dhingra@users.noreply.github.com>
2023-07-22 00:33:15 +0530

    Add files via upload

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,25 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage="dice"+randomNumber1+".png";
+var randomImageSource="images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2="dice"+randomNumber2+".png";
+var randomImageSource2="images/"+randomDiceImage2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h4").innerHTML="You Win";
+
+}
+else if(randomNumber1==randomNumber2){
+    document.querySelector("h4").innerHTML="Draw";
+}
+else{
+    document.querySelector("h4").innerHTML="You Lost";
+}
+
